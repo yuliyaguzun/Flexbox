25573f1257208cef3b87b4c05b1ef05ba7c97985
Yuliya <yliguzun@gmail.com>
2019-01-13 22:28:29 +0300

    Кейс №2 Перестановка карточек

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 $(function () {
     console.log('hhhh');
-    $('.btn-holder_item_purple').click(function () {
+    $('#transfom1').click(function () {
         $('.case_holder').toggleClass('justify-content-start');
         $('.case_holder').removeClass('justify-content-between');
         $('.case_holder').removeClass('justify-content-end');
         $('.case_holder').removeClass('direction-column');
     });
-    $('.btn-holder_item_orange').click(function () {
+    $('#transfom2').click(function () {
         $('.case_holder').toggleClass('justify-content-between');
     });
-    $('.btn-holder_item_green').click(function () {
+    $('#transfom3').click(function () {
         $('.case_holder').toggleClass('justify-content-end');
     });
-    $('.btn-holder_item_violet').click(function () {
+    $('#transfom4').click(function () {
         $('.case_holder').toggleClass('direction-column');
+    });
+    $('#transfom5').click(function () {
+        $('.case_card_long:last-child').css('order', '-5');
+        $('.case_card_long:first-child').css('order', '5');
+    });
+    $('#transfom6').click(function () {
+        $('.case_card_long:nth-child(2)').toggleClass('down-position');
+        $('.case_card_long:nth-child(3)').toggleClass('down-position');
     });
 });
